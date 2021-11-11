581599974852df6efdb1404de41f439ec2beb02b
Reginaldo <florentinolim@gmail.com>
2021-11-10 23:15:44 -0300

    Resolvendo os exercicios 87 88 Lista composta

--- TerceiroMundo/Listas2/Exercicios/87 AprimoreOdesafio86.py
+++ TerceiroMundo/Listas2/Exercicios/87 AprimoreOdesafio86.py
@@ -4,14 +4,26 @@ B)A soma dos valores da terceira coluna
 c)O maior valor da segunda coluna
 '''
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+soma = 0
+scol = 0
+maiorc = 0
 for l in range(0, 3):
     for c in range(0, 3):
         matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
 print('-=' * 30)
 for l in range(0, 3):
     for c in range(0, 3):
-        if c % 2 == 0:
-            
         print(f'[{matriz[l][c]:^5}]', end='')
-
+        if matriz[l][c] % 2 == 0:  # Soma de todos os pares digitados
+            soma += matriz[l][c]
     print()
+print(f'A soma dos pares são: {soma}')
+for l in range(0, 3):
+    scol += matriz[l][2]
+print(f'A soma da terceira coluna é {scol}')
+for c in range(0, 3):
+    if c == 0:
+        maior = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+print(f'O maior valor da segunda linha é: {maior}')
--- TerceiroMundo/Listas2/Exercicios/88 MegaSena.py
+++ TerceiroMundo/Listas2/Exercicios/88 MegaSena.py
@@ -1,2 +1,38 @@
-'''Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai
-sortear 6 numeros entre 1 e 60 para cada jogo, cadastrando tudo em uma unica lista composta'''
+'''Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+O programa vai perguntar quantos jogos serão gerados e vai sortear 6 numeros entre 1 e 60 para cada jogo,
+cadastrando tudo em uma unica lista composta'''
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+qtnumeros = 0
+print('-' * 30)
+print('-     JOGOS DA MEGA SENA     ')
+print('-' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+qtnumeros = int(input('Quantos numeros por cartão? '))
+valor = float(input('Informe o valor de cada jogo '))
+total = quant * valor
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= qtnumeros:
+           break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'  SORTEANDO {quant} JOGOS ',  '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'jogo {i+1}: {l}')
+    sleep(1)
+print('-=' * 5, '< BOA SORTE! >', '-=' * 5)
+print(f'Total a pagar R${total}')
+
+
+
